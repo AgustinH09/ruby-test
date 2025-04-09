ae63dcb4ede7c59edff9476d49a3cb61ac760fba
AgustinH09 <agus.hernandez9@gmail.com>
2025-04-09 09:46:46 -0300

    Initial setup

---
+++ test/controllers/health_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class HealthControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get health_index_url
+    assert_response :success
+  end
+end
